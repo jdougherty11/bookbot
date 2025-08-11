8d653c4bd0dc99130b1c2c6b20971125a218d609
jdougherty11 <joedougherty30@hotmail.com>
2025-08-11 14:21:06 -0400

    B:added function count number of characters and sort character count from most to least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_characters
+from stats import sort_character_count
 
 
 def get_book_text(filepath):
@@ -11,7 +13,11 @@ def main():
     filepath = "books/frankenstein.txt"
     book_contents = get_book_text(filepath)
     word_count = count_words(book_contents)
+    char_count = count_characters(book_contents)
+    sorted_count = sort_character_count(char_count)
     print(f"{word_count} words found in the document")
+    print(char_count)
+    print(sorted_count)
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    char_count = {}
+    for char in text:
+        lowercase_char = char.lower()
+        char_count[lowercase_char] = char_count.get(lowercase_char, 0) + 1
+    return char_count
+
+def sort_on(items):
+    return items["num"]
+
+def sort_character_count(char_count):
+    sorted_count = []
+    for key, value in char_count.items():
+        char_dict = {"char": key, "num": value}
+        sorted_count.append(char_dict)
+    sorted_count.sort(key=sort_on, reverse=True)
+    return sorted_count
